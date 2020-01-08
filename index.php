4eb9d363ffd0ec5ccb4b0a584b5eee08d3fe1b63
Matt Moran <mattmoran2000@gmail.com>
2020-01-08 15:05:41 +1300

    Update index.php

--- index.php
+++ index.php
@@ -11,20 +11,26 @@ if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
 
-$jwt = !empty($_GET['jwt']) ? $_GET['jwt'] : 'unset';
+$jwt = !empty($_GET['jwt']) ? "jwt='$_GET[jwt]'" : '';
+$id = !empty($_GET['id']) ? "id='$_GET[id]'" : '';
 
-if ($jwt !== 'unset') {
+if ($jwt && $id) {
     // sql injection
-    $sql = "SELECT id FROM main WHERE jwt='$jwt'";
+    $sql = "SELECT jwt, id FROM main WHERE ($jwt) AND ($id)";
+}
+else if ($jwt || $id) {
+    // sql injection
+    $sql = "SELECT jwt, id FROM main WHERE $jwt $id";
 }
 else {
     $sql = 'SELECT * FROM main';
 }
+echo $sql . '<br>' . PHP_EOL;
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo '$_GET[\'jwt\']: ' . $jwt . ' - id: ' . $row['id'] . '<br>' . PHP_EOL;
+        echo 'jwt=' . $row['jwt'] . '; id=' . $row['id'] . '<br>' . PHP_EOL;
     }
 } else {
     echo 'No results';
